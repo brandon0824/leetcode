ea0e53d20df27cd56c89d65d089bf66ad4a1d190
brandon0824 <446038117@qq.com>
2020-08-30 11:32:47 +0800

    leetcode141 lcof23

---
+++ 141.环形链表.java
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null){
+            return false;
+        }
+        ListNode first = head;
+        ListNode second = head.next;
+        while(first != second){
+            if(second == null || second.next == null){
+                return false;
+            }
+            first = first.next;
+            second = second.next.next;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
