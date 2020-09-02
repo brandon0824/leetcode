67518ce1a138256937fcbc50343bb1e93fd4a1f8
brandon0824 <446038117@qq.com>
2020-09-02 23:46:42 +0800

    leetcode138 lcof35

---
+++ 138.复制带随机指针的链表.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=138 lang=java
+ *
+ * [138] 复制带随机指针的链表
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        
+    }
+}
+// @lc code=end
+
