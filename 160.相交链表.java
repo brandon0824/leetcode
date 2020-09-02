f6841431a2782a53b50e6db9a3b6783d50e20530
brandon0824 <446038117@qq.com>
2020-09-02 23:47:05 +0800

    leetcode160 lcof52

---
+++ 160.相交链表.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=160 lang=java
+ *
+ * [160] 相交链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+    }
+}
+// @lc code=end
+
