77c2f1989957977979d5bb5747e2777cd7e3ae70
brandon0824 <446038117@qq.com>
2020-09-03 14:25:09 +0800

    leetcode138 lcof35

--- 138.复制带随机指针的链表.java
+++ 138.复制带随机指针的链表.java
@@ -22,8 +22,49 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        
+        if(head == null){
+            return null;
+        }
+        copyNode(head);
+        randomDirect(head);
+        return reList(head);
     }
+
+    private void copyNode(Node head){
+        while(head != null){
+            Node nextNode = head.next;
+            Node newNode = new Node(head.val);
+            head.next = newNode;
+            newNode.next = nextNode;
+            head = nextNode;
+        }
+    }
+
+    private void randomDirect(Node head){
+        while(head != null){
+            Node nextNode = head.next;
+            if(head.random != null){
+                Node direct = head.random;
+                nextNode.random = direct.next;
+            }
+            head = nextNode.next;
+        }
+    }
+
+    private Node reList(Node head){
+        Node newNode = head.next;
+        Node newHead = newNode;
+        head.next = newNode.next;
+        head = head.next;
+        while(head != null){
+            newNode.next = head.next;
+            head.next = head.next.next;
+            head = head.next;
+            newNode = newNode.next;
+        }
+        return newHead;
+    }
+
 }
 // @lc code=end
 
