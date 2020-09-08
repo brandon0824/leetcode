f4e3f4b1f1142f64a125a8dd590bb3aea2d15fa5
brandon0824 <446038117@qq.com>
2020-09-08 17:31:46 +0800

    leetcode946 lcof31

---
+++ 946.验证栈序列.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=946 lang=java
+ *
+ * [946] 验证栈序列
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for(int num : pushed){
+            stack.push(num);
+            while(!stack.isEmpty() && stack.peek() == popped[i]){
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
+
