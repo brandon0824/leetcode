8ddeade3764e689305d8798a6af02161b20c170c
brandon0824 <446038117@qq.com>
2020-09-10 14:20:29 +0800

    leetcode105 lcof07

---
+++ 105.从前序与中序遍历序列构造二叉树.java
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=105 lang=java
+ *
+ * [105] 从前序与中序遍历序列构造二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return preorder;
+    }
+}
+// @lc code=end
+
