36672207c8048d4d535a3d760906c0157098d86d
brandon0824 <446038117@qq.com>
2020-09-10 22:19:08 +0800

    leetcode105 lcof07

--- 105.从前序与中序遍历序列构造二叉树.java
+++ 105.从前序与中序遍历序列构造二叉树.java
@@ -16,7 +16,31 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return preorder;
+        int n = preorder.length;
+        int m = inorder.length;        
+        if(m == 0 || m == 0 || m != n){
+            return null;
+        }
+
+        Map<Integer, Integer> indexMap = new HashMap<>(n);
+        for(int i = 0; i < n; i++){
+            indexMap.put(inorder[i], i);
+        }
+        return constructTree(preorder, 0, n-1, indexMap, 0, n-1);
+    }
+
+    private TreeNode constructTree(int[] pre, int startPre, int endPre,
+                                    Map<Integer, Integer> indexMap, int startIn, int endIn){
+        if(startPre > endPre || startIn > endIn){
+            return null;
+        }
+        
+        int rootval = pre[startPre];
+        TreeNode node = new TreeNode(rootval);
+        int rootIndex = indexMap.get(rootval);
+        node.left = constructTree(pre, startPre+1, rootIndex-startIn+startPre, indexMap, startIn, rootIndex-1);
+        node.right = constructTree(pre, rootIndex-startIn+startPre+1, endPre, indexMap, rootIndex+1, endIn);
+        return node;
     }
 }
 // @lc code=end
