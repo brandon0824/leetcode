cc4879b11211da49fbb992f46a16c6d48bc95103
brandon0824 <446038117@qq.com>
2020-09-22 17:28:27 +0800

    leetcode226 lcof27

---
+++ 226.翻转二叉树.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=226 lang=java
+ *
+ * [226] 翻转二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+        
+    }
+}
+// @lc code=end
+
