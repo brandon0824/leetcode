72c4137afe175b3925f24ee7d6df77d8892a3678
brandon0824 <446038117@qq.com>
2020-10-01 11:21:08 +0800

    lcof32-3

---
+++ 32-3.从上到下打印二叉树.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(root != null)
+            queue.add(root);
+        while(!queue.isEmpty()){
+            List<Integer> tmp = new ArrayList<>();
+            for(int i = queue.size(); i > 0; i--){
+                TreeNode node = queue.poll();
+                tmp.add(node.val);
+                if(node.left != null)
+                    queue.add(node.left);
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+            if(res.size() % 2 == 1)
+                Collections.reverse(tmp);
+            res.add(tmp);
+        }
+        return res;
+    }
+}
