b08ee7d74aa34d5175b3b67e5362a697f9fb9d0a
brandon0824 <446038117@qq.com>
2022-06-19 20:33:59 +0800

    update

---
+++ 20.有效的括号.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        if(s.isEmpty()){
+            return true;
+        }
+        Stack<Character> stack = new Stack<Character>();
+        for(char c:s.toCharArray()){
+            if(c == '(') stack.push(')');
+            else if(c == '[') stack.push(']');
+            else if(c == '{') stack.push('}');
+            else if(stack.isEmpty() || c != stack.pop()){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+// @lc code=end
