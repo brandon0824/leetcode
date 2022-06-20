96f10736743cff537d0f8fedc56c49c51c3b40e3
brandon0824 <446038117@qq.com>
2022-06-20 11:08:28 +0800

    update

--- 20.有效的括号.java
+++ 20.有效的括号.java
@@ -7,19 +7,15 @@
 // @lc code=start
 class Solution {
     public boolean isValid(String s) {
-        if(s.isEmpty()){
-            return true;
-        }
         Stack<Character> stack = new Stack<Character>();
         for(char c:s.toCharArray()){
             if(c == '(') stack.push(')');
             else if(c == '[') stack.push(']');
             else if(c == '{') stack.push('}');
-            else if(stack.isEmpty() || c != stack.pop()){
-                return false;
-            }
+            else if(stack.isEmpty() || c != stack.pop()) return false;
         }
         return stack.isEmpty();
     }
 }
 // @lc code=end
+
