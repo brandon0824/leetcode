6c4f5e2de6175602ebb87ef5a89ef4ea6733dcda
brandon0824 <446038117@qq.com>
2022-06-21 11:28:25 +0800

    update

--- 155.最小栈.java
+++ 155.最小栈.java
@@ -6,40 +6,38 @@
 
 // @lc code=start
 class MinStack {
-
-    Stack<Integer> dataStack,minStack;
-    /** initialize your data structure here. */
+    Stack<Integer> stack, min_stack;
     public MinStack() {
-        dataStack = new Stack<>();
-        minStack = new Stack<>();
+        stack = new Stack<>();
+        min_stack = new Stack<>();
     }
     
-    public void push(int x) {
-        dataStack.push(x);
-        if(minStack.isEmpty() || x <= minStack.peek()){
-            minStack.push(x);
+    public void push(int val) {
+        stack.add(val);
+        if(min_stack.empty() || min_stack.peek() >= val) {
+            min_stack.add(val);
         }
     }
     
     public void pop() {
-        if(dataStack.pop().equals(minStack.peek())){
-            minStack.pop();
+        if(stack.pop().equals(min_stack.peek())){
+            min_stack.pop();
         }
     }
     
     public int top() {
-        return dataStack.peek();
+        return stack.peek();
     }
     
     public int getMin() {
-        return minStack.peek();
+        return min_stack.peek();
     }
 }
 
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
- * obj.push(x);
+ * obj.push(val);
  * obj.pop();
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
