4eb7762eb87d1bf4312d079115d5ac2e68ef7423
brandon0824 <446038117@qq.com>
2022-07-17 21:59:06 +0800

    update

---
+++ 50.pow-x-n.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=50 lang=java
+ *
+ * [50] Pow(x, n)
+ */
+
+// @lc code=start
+class Solution {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        long b = n;
+        double res = 1.0;
+        if(b < 0){
+            x = 1/x;
+            b = -b;
+        }
+        while(b > 0){
+            if((b & 1) == 1){
+                res *= x;
+            }
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
