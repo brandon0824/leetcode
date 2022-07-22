0262ed81309fe13e0b148daee3ca02d9e3e6dbb8
brandon0824 <446038117@qq.com>
2022-07-22 12:36:15 +0800

    update

--- 102.二叉树的层序遍历.java
+++ 102.二叉树的层序遍历.java
@@ -11,29 +11,32 @@
  *     int val;
  *     TreeNode left;
  *     TreeNode right;
- *     TreeNode(int x) { val = x; }
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        List<List<Integer>> res = new ArrayList<>();
-        if(root != null){
-            queue.add(root);
-        }
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null) return res;
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
         while(!queue.isEmpty()){
-            List<Integer> tmp = new ArrayList<>();
-            for(int i = queue.size(); i>0; i--){
-                TreeNode node = queue.poll();
-                tmp.add(node.val);
-                if(node.left != null){
-                    queue.add(node.left);
-                }
-                if(node.right != null){
-                    queue.add(node.right);
-                }
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevel = queue.size();
+            for(int i = 1; i <= currentLevel; i++){
+                TreeNode temp = queue.poll();
+                level.add(temp.val);
+                if(temp.left != null) queue.add(temp.left);
+                if(temp.right != null) queue.add(temp.right);
             }
-            res.add(tmp);
+            res.add(level);
         }
         return res;
     }
