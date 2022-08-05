b215b19a319b8fc7909894c3d352c4ba7aabd930
brandon0824 <446038117@qq.com>
2022-08-05 23:17:30 +0800

    update

---
+++ 300.最长递增子序列.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        if(nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int res = 0;
+        for(int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+}
+// @lc code=end
+
