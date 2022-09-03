4e4bac6398fa7412f9f92a0abe5889e117cd50cb
brandon0824 <446038117@qq.com>
2022-09-03 21:26:35 +0800

    update

---
+++ 23.合并k个升序链表.java
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=23 lang=java
+ *
+ * [23] 合并K个升序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                if(o1.val < o2.val) return -1;
+                else if (o1.val == o2.val) return 0;
+                else return 1;
+            }
+        });
+        ListNode dummy = new ListNode(0);
+        ListNode p = dummy;
+        for(ListNode list:lists){
+            if(list != null) queue.add(list);
+        }
+        while(!queue.isEmpty()){
+            p.next = queue.poll();//队首 最小值
+            p = p.next;
+            if(p.next != null) queue.add(p.next);
+        }
+        return dummy.next;
+    }
+}
+// @lc code=end
+
