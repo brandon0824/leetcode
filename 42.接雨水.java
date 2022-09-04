32cc0cd9208f0f764105839b15a068c15a3fc5b8
brandon0824 <446038117@qq.com>
2022-09-04 23:44:41 +0800

    update

---
+++ 42.接雨水.java
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=42 lang=java
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+class Solution {
+    public int trap(int[] height) {
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int res = 0;
+        while(left < right){
+            leftMax = Math.max(leftMax, height[left]);
+            rightMax = Math.max(rightMax, height[right]);
+            if(height[left] < height[right]){
+                res += leftMax - height[left];
+                left++;
+            }
+            else{
+                res += rightMax - height[right];
+                right--;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
